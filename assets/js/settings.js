ccec75e93b0ae9f1797eb9cd779befec402f3f8e
rmigold <golddragon@gmail.com>
2019-07-20 18:43:32 -0700

    "2019-07-20 18:43"

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -18,6 +18,27 @@ jQuery( document ).ready(
 
 		$( '.color' ).click(
 			function () {
+				$( this ).closest( '.color-picker' ).find( '.colorpicker' ).fadeIn();
+			}
+		);
+
+		$( document ).mousedown(
+			function () {
+				$( '.colorpicker' ).each(
+					function () {
+						var display = $( this ).css( 'display' );
+						if (display == 'block') {
+							$( this ).fadeOut();
+						}
+					}
+				);
+			}
+		);
+
+		/***** Uploading images *****/
+
+		var file_frame;
+
 		jQuery.fn.uploadMediaFile = function (button, preview_media) {
 			var button_id  = button.attr( 'id' );
 			var field_id   = button_id.replace( '_button', '' );
